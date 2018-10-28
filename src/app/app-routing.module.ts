45b87221eb0955bbff4dea7bb3bf45a7ff2fbb55
nataraj boina <nbnataraj7@gmail.com>
2018-10-28 19:41:12 +0530

    More changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DevicesComponent } from './devices/devices.component';
+import { AppsComponent } from './apps/apps.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { ThemesComponent } from './themes/themes.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  children: [{
+    path: 'devices',
+    component: DevicesComponent
+  }, {
+    path: 'profiles',
+    component: ProfilesComponent
+  }, {
+    path: 'themes',
+    component: ThemesComponent
+  }, {
+    path: 'apps',
+    component: AppsComponent
+  }]
+}, {
+  path: 'login',
+  component: LoginPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DevicesComponent } from './devices/devices.component';
+import { AppsComponent } from './apps/apps.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { ThemesComponent } from './themes/themes.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginPageComponent,
+    DevicesComponent,
+    AppsComponent,
+    ProfilesComponent,
+    ThemesComponent,
+    NavbarComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
